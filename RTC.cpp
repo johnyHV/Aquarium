2067687dec172fc01da92de11f7d8e31ac7de710
Miroslav <miroslav.pivovarsky@iklub.sk>
2014-09-03 09:35:54 +0200

    Initial

---
+++ RTC.cpp
@@ -0,0 +1,101 @@
+#include "RTC.h"
+
+//------------------------------------------------------------------------------
+
+/**
+ *
+ * @info Nastavi cas na RTC
+ * @param time - struktura naplnena casom pre zapis
+ * @return void
+ */
+void setTime(time_date times) {
+    Wire.begin();
+    Wire.beginTransmission(DS1307);
+    Wire.write(byte(0));
+    Wire.write(decToBcd(times.sec));
+    Wire.write(decToBcd(times.min));
+    Wire.write(decToBcd(times.hour));
+    Wire.write(decToBcd(times.week_day));
+    Wire.write(decToBcd(times.month_day));
+    Wire.write(decToBcd(times.month));
+    Wire.write(decToBcd(times.year));
+    Wire.write(byte(0));
+    Wire.endTransmission();
+}
+
+//------------------------------------------------------------------------------
+
+/**
+ *
+ * @info Vycita cas z RTC
+ * @param 
+ * @return void
+ */
+ time_date readTime() {
+    
+  time_date times = {0,0,0,0,0,0,0};
+    Wire.beginTransmission(DS1307);
+    Wire.write(byte(0));
+    Wire.endTransmission();
+    Wire.requestFrom(DS1307, 7);
+    times.sec = bcdToDec(Wire.read());
+    times.min = bcdToDec(Wire.read());
+    times.hour = bcdToDec(Wire.read());
+    times.week_day = bcdToDec(Wire.read());
+    times.month_day = bcdToDec(Wire.read());
+    times.month = bcdToDec(Wire.read());
+    times.year = bcdToDec(Wire.read());
+    return times;
+}
+
+
+//------------------------------------------------------------------------------
+
+/**
+ *
+ * @info Vypise cas na konzolu
+ * @param none
+ * @return void
+ */
+void printTime(time_date times) {
+    Serial.print("Date & time: ");
+    Serial.print(times.month_day);
+    Serial.print(" ");
+    Serial.print(times.month_day);
+    Serial.print(".");
+    Serial.print(times.month);
+    Serial.print(".");
+    Serial.print(times.year);
+    Serial.print(" ");
+    Serial.print(times.hour);
+    Serial.print(":");
+    Serial.print(times.min);
+    Serial.print(":");
+    Serial.print(times.sec);
+    Serial.println();
+}
+//------------------------------------------------------------------------------
+
+/**
+ *
+ * @info Dec to Bcd
+ * @param hodnota
+ * @return byte
+ */
+byte decToBcd(byte val) {
+    return ((val / 10 * 16) + (val % 10));
+}
+
+//------------------------------------------------------------------------------
+
+/**
+ *
+ * @info Bcd to Dec
+ * @param hodnota
+ * @return byte
+ */
+byte bcdToDec(byte val) {
+    return ((val / 16 * 10) + (val % 16));
+}
+
+
---
+++ RTC.h
@@ -0,0 +1,28 @@
+#ifndef __RTC_H
+#define __RTC_H
+
+#include <Arduino.h>
+#include <Wire.h>
+
+struct time_date {
+    uint8_t hour; // hodiny
+    uint8_t min; // minuty
+    uint8_t sec; // sekundy
+
+    uint8_t week_day; // den v tyzdni
+    uint8_t month_day; // den v mesiaci
+    uint8_t month; // mesiac
+    uint8_t year; // rok
+};
+
+const int DS1307 = 0x68; // Address of DS1307 see data sheets
+// const char* days[] ={"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+// const char* months[] ={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+byte decToBcd(byte val);
+byte bcdToDec(byte val);
+void setTime(time_date);
+time_date readTime();
+void printTime(time_date);
+
+#endif
